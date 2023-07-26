48eedcc482de26b02984ade5ce1c54247921b701
quangvih12 <vinhnqph20805@fpt.edu.vn>
2023-07-26 12:14:28 +0700

    delete

--- thongKe.sql
+++
@@ -1,9 +0,0 @@
-
-// san pham ban cham va chay nhat
-select  pt.id_chi_tietsp,COUNT(*) AS tutor_sold, SUM(PT.so_luong)AS soluong from hoa_don_chi_tiet pt join hoa_don hd on pt.id_hoa_don = hd.id
-where hd.trang_thai_tt = -1
-GROUP BY pt.id_chi_tietsp
-ORDER BY tutor_sold DESC
-
-
-
